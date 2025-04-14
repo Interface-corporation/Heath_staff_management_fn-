e8d33ef989fc70ba388e964b4451b4cabcf756f8
IGIRIMPUHWE Dositha <igirimpuhwedosta@gmail.com>
2025-04-14 10:05:03 +0200

    ft:added frontend health staff manager app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import LandingPage from './pages/landing/LandingPage'
+
+
+
+
+const App=() => {
+
+
+  return (
+   <div className='font-[Poppins]'>
+  
+    <LandingPage/>
+    </div> 
+   
+  )
+}
+
+export default App
